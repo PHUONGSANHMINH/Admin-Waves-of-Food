50b614741c647586a4c96de39ed1e399482cdaa2
Trần Lê Công Ti <congti1812@gmail.com>
2024-05-20 15:40:41 +0700

    8:01:33

--- app/src/main/java/com/example/admin/AddItemActivity.kt
+++ app/src/main/java/com/example/admin/AddItemActivity.kt
@@ -2,7 +2,9 @@ package com.example.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.admin.databinding.ActivityAddItemBinding
 
 class AddItemActivity : AppCompatActivity() {
@@ -13,6 +15,17 @@ class AddItemActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         binding.selectImage.setOnClickListener{
+            pickiamge.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+        }
+        binding.backButton.setOnClickListener {
+            finish()
         }
     }
+
+    val pickiamge =
+        registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
+            if (uri != null) {
+                binding.selectedImage.setImageURI(uri)
+            }
+        }
 }
---
+++ app/src/main/java/com/example/admin/adapter/AddItemAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.admin.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.admin.databinding.ItemItemBinding
+
+class AddItemAdapter(private val MenuItemName: ArrayList<String>, private val MenuItemPrice: ArrayList<String>, private val MenuItemImage: ArrayList<Int>): RecyclerView.Adapter<AddItemAdapter.AddItemViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AddItemViewHolder {
+        val binding = ItemItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return AddItemViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: AddItemViewHolder, position: Int) {
+        holder.bind(position)
+    }
+
+    override fun getItemCount(): Int = MenuItemName.size
+
+    inner class AddItemViewHolder(private val binding: ItemItemBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(position: Int) {
+            binding.apply {
+                foodnameTextView.text = MenuItemName[position]
+                priceTextView.text = MenuItemPrice[position]
+                foodImageView.setImageResource(MenuItemImage[position])
+            }
+        }
+
+    }
+}
