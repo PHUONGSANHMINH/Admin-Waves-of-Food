7bb3e4f3c0153232a48121526ef42aeda4756a9b
nhikhanhh <khanhnhi931819@gmail.com>
2024-06-05 17:31:04 +0700

    2:08:55

--- app/src/main/java/com/example/admin/AllItemActivity.kt
+++ app/src/main/java/com/example/admin/AllItemActivity.kt
@@ -2,35 +2,66 @@ package com.example.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.admin.adapter.AddItemAdapter
+import com.example.admin.adapter.MenuItemAdapter
 import com.example.admin.databinding.ActivityAllItemBinding
+import com.example.admin.model.AllMenu
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 class AllItemActivity : AppCompatActivity() {
+
+    private lateinit var databaseReference: DatabaseReference
+    private lateinit var database: FirebaseDatabase
+    private var menuItems: ArrayList<AllMenu> = ArrayList()
+
     private val binding: ActivityAllItemBinding by lazy {
         ActivityAllItemBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        val menuFoodName = listOf("Burger","sandwich","momo","item","sandwich","momo")
-        val menuItemPrice = listOf("$5","$6","$8","$9","$10","$10")
-        val menuImage = listOf(
-            R.drawable.menu1,
-            R.drawable.menu2,
-            R.drawable.menu3,
-            R.drawable.menu4,
-            R.drawable.menu5,
-            R.drawable.menu4
-        )
+        databaseReference = FirebaseDatabase.getInstance().reference
+        retrieveMenuItem()
+
         binding.backButton.setOnClickListener {
             finish()
         }
-        val adapter = AddItemAdapter(
-            ArrayList(menuFoodName),
-            ArrayList(menuItemPrice),
-            ArrayList(menuImage)
-        )
+
+    }
+
+    private fun retrieveMenuItem() {
+        database = FirebaseDatabase.getInstance()
+        val foodRef : DatabaseReference = database.reference.child("menu")
+
+        //fetch data from database
+        foodRef.addListenerForSingleValueEvent(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                // Clear existing data before populating
+                menuItems.clear()
+
+                // loop for through each food item
+                for (foodSnapshot in snapshot.children){
+                    val menuItem  = foodSnapshot.getValue(AllMenu::class.java)
+                    menuItem?.let {
+                        menuItems.add(it)
+                    }
+                }
+                setAdapter()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("DatabaseError", "Error: ${error.message} ")
+            }
+
+        })
+    }
+    private fun setAdapter() {
+        val adapter = MenuItemAdapter(this@AllItemActivity, menuItems, databaseReference)
         binding.MenuRecyclerView.layoutManager = LinearLayoutManager(this)
         binding.MenuRecyclerView.adapter = adapter
     }
